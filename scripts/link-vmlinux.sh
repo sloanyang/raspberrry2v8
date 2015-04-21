c22307bbc7a95f7098f31a71cb5f46a744283495
sloanyang <sloanyang@gmail.com>
2015-04-21 17:49:38 +0800

    [What] Fixing bugs.

--- scripts/link-vmlinux.sh
+++ scripts/link-vmlinux.sh
@@ -102,7 +102,7 @@ kallsyms()
 # See mksymap for additional details
 mksysmap()
 {
-	${CONFIG_SHELL} "${srctree}/scripts/mksysmap" ${1} ${2}
+	${CONFIG_SHELL} "${roottree}/scripts/mksysmap" ${1} ${2}
 }
 
 sortextable()
@@ -153,7 +153,7 @@ info LD vmlinux.o
 modpost_link vmlinux.o
 
 # modpost vmlinux.o to check for section mismatches
-${MAKE} -f "${srctree}/scripts/Makefile.modpost" vmlinux.o
+${MAKE} -f "${roottree}/scripts/Makefile.modpost" vmlinux.o
 
 # Update version
 info GEN .version
@@ -166,7 +166,7 @@ else
 fi;
 
 # final build of init/
-${MAKE} -f "${srctree}/scripts/Makefile.build" obj=init
+${MAKE} -f "${roottree}/scripts/Makefile.build" obj=init
 
 kallsymso=""
 kallsyms_vmlinux=""
