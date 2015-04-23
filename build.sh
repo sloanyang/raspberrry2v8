c5a79c3006a4671876a95494f5af410d9ad8ff78
sloanyang <sloanyang@gmail.com>
2015-04-23 11:13:34 +0800

    [What] Fixing bugs in makefile.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 export KBUILD_VERBOSE=1
 #export KBUILD_OUTPUT=obj 
 export KBUILD_CHECKSRC=1
-make ARCH=arm CROSS_COMPILE=arm-bcm2708-linux-gnueabi- menuconfig
+make ARCH=arm CROSS_COMPILE=arm-bcm2708-linux-gnueabi- bcmrpi_defconfig
 #make ARCH=arm CROSS_COMPILE=arm-bcm2708-linux-gnueabi- clean
-#make ARCH=arm CROSS_COMPILE=arm-bcm2708-linux-gnueabi-
+make ARCH=arm CROSS_COMPILE=arm-bcm2708-linux-gnueabi-
